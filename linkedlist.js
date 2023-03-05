536b6818c8a4acecf6439e6c568d49b1e3ca5fc6
vvillbii <will.bowles90@gmail.com>
2023-03-05 15:19:40 -0500

    add push method to linked list

--- linkedlist.js
+++ linkedlist.js
@@ -12,4 +12,17 @@ class LinkedList {
     this.tail = this.head;
     this.length = 1;
   }
+
+  push(value) {
+    const newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
 }
